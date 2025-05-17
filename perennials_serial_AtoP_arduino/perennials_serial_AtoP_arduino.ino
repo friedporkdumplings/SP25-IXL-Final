c12fe86b82062b1017c25314a596686a9281ae38
Jae Huang <128411957+friedporkdumplings@users.noreply.github.com>
2025-05-17 23:16:38 +0800

    updates

--- perennials_serial_AtoP_arduino/perennials_serial_AtoP_arduino.ino
+++ perennials_serial_AtoP_arduino/perennials_serial_AtoP_arduino.ino
@@ -1,18 +1,23 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(7, INPUT);
+  pinMode(5, INPUT);
 }
 
 void loop() {
   // to send values to Processing assign the values you want to send
   // this is an example:
-  int sensor0 = analogRead(A0);
-  int sensor1 = analogRead(A1);
+  int sensor0 = digitalRead(7);
+  int sensor1 = digitalRead(5);
+  int sensor2 = analogRead(A2);
 
   // send the values keeping this format
   Serial.print(sensor0);
-  Serial.print(",");  // put comma between sensor values
+  Serial.print(",");
   Serial.print(sensor1);
-  Serial.println();  // add linefeed after sending the last sensor value
+  Serial.print(",");
+  Serial.print(sensor2);   
+  Serial.println();
 
   // too fast communication might cause some latency in Processing
   // this delay resolves the issue
